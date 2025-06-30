5df6862f23e3cc66b9ce663966362669d17e2d95
haochend413 <dinghaochen413@gmail.com>
2025-06-30 02:54:07 -0500

    feat: add cls model

--- server/main.py
+++ server/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-from server.API.run import model_router
+from API.run import model_router
 
 
-app = FastAPI()
+app = FastAPI() 
 app.include_router(model_router)
 
 
 @app.get("/")
-def root():
+def root(): 
     return {"Auto Weed Image Processing"}

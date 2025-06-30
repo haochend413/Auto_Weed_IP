bd1e2447670e90aa0b86efcee936a1fa33f2ed69
haochend413 <dinghaochen413@gmail.com>
2025-06-30 00:53:50 -0500

    feat: add support for classification

--- server/API/run.py
+++ server/API/run.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile, File, APIRouter, BackgroundTasks
+from fastapi import FastAPI, UploadFile, File, APIRouter, BackgroundTasks,Path as FastAPIPath
 from fastapi.responses import JSONResponse, StreamingResponse
 from ultralytics import YOLO
 import tempfile
@@ -7,17 +7,28 @@ import zipfile
 import shutil
 import io
 
-detect_router = APIRouter()
+model_router = APIRouter()
 
 
-@detect_router.post("/detect")
-def detect(background_tasks: BackgroundTasks, images: list[UploadFile] = File(...)):
+@model_router.post("/{operation}") 
+def detect( background_tasks: BackgroundTasks,
+           operation: str = FastAPIPath(..., description="Model Type"), 
+           images: list[UploadFile] = File(...)):
     print("request received")
     try:
         # is this right? maybe
         # create a temp dir for auto-deletion
-        model_path = Path(__file__).parent.parent / "models" / "detection" / "best.pt" # Correct path expansion
+        # Choose model path based on operation 
+        if operation == "detect":
+            model_path = Path(__file__).parent.parent / "models" / "detection" / "best.pt"
+        elif operation == "segment":
+            model_path = Path(__file__).parent.parent / "models" / "segment" / "best.pt"
+        elif operation == "classify":
+            model_path = Path(__file__).parent.parent / "models" / "classify" / "best.pt"
+        else:
+            return JSONResponse(status_code=400, content={"error": f"Unknown operation: {operation}"})
         model = YOLO(model_path)
+
         with tempfile.TemporaryDirectory() as tmpdir:
             tmpdir_path = Path(tmpdir)
 
@@ -29,10 +40,9 @@ def detect(background_tasks: BackgroundTasks, images: list[UploadFile] = File(..
             result = model(source=str(tmpdir_path), save=True)
 
             # output result
-            # this might work ?
             output_dir = Path(__file__).parent.parent.parent / "runs"
 
-            # buffer to store & send zip file
+            # buffer to store & send zip file 
             zip_buffer = io.BytesIO()
             with zipfile.ZipFile(zip_buffer, "w") as zip_file:
                 for file_path in output_dir.rglob("*"):

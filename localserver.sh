d2e6bb361711ba730c74ab108a2916de5030e720
haochend413 <dinghaochen413@gmail.com>
2025-08-11 14:52:32 -0500

    tag: v1.0.0

--- localserver.sh
+++ localserver.sh
@@ -13,7 +13,6 @@ done
 echo "Activating virtual environment..."
 source venv/bin/activate
 # mkcert will not work for distribution.
-
 echo "Starting local server..."
 uvicorn main:app \
     --host 0.0.0.0 \
